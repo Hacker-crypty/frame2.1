bf47846cd8992aa8be6654d7a1067c8b9de17782
Solares <66849017+Hacker-crypty@users.noreply.github.com>
2024-10-07 14:16:01 +0000

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,12 @@
 import footer from "./modulos/footer";
-import navbar from "./modulos/navbar";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import menu from "./modulos/navbar";
+import { Container, Row, Col } from 'react-bootstrap';
 
 export default function Index() {
   return <div>
-    <navbar />
+    <menu />
     Bom dia
-    <Container>
-      <Row>
-        <Col className="bg-success">1 of 2</Col>
-        <Col>2 of 2</Col>
-      </Row>
-      <Row>
-        <Col>1 of 3</Col>
-        <Col>2 of 3</Col>
-        <Col>3 of 3</Col>
-      </Row>
-    </Container>
+    
+   <footer />
   </div>
 }
--- src/pages/modulos/navbar.js
+++ src/pages/modulos/navbar.js
@@ -1,4 +1,18 @@
-function navbar() {
-    return <div>MENU</div>
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
+export default function menu() {
+    return (
+        <Navbar expand="sm" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
+            <Container>
+                <Navbar.Brand href="#home">6A</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Produtos</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
-export default navbar;
